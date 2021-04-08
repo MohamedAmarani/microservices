cacba86c2b444026066262adf351d64092bdab8e
MohamedAmarani <mohamedamarani@inslauro.net>
2021-04-08 16:30:38 +0200

    M

--- account-service/src/main/java/com/users/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/com/users/accountservice/AccountServiceApplication.java
@@ -3,6 +3,7 @@ package com.users.accountservice;
 import com.users.accountservice.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +13,8 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
+
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
