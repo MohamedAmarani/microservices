dc12863087c704c47a3a8e488985699aed6ef363
MohamedAmarani <mohamedamarani@inslauro.net>
2021-04-23 16:41:24 +0200

    m

--- delivery-service/src/main/java/com/ecommerce/deliveryservice/controller/HomeController.java
+++ delivery-service/src/main/java/com/ecommerce/deliveryservice/controller/HomeController.java
@@ -3,7 +3,9 @@ package com.ecommerce.deliveryservice.controller;
 import com.ecommerce.deliveryservice.model.Cart;
 import com.ecommerce.deliveryservice.model.Delivery;
 import com.ecommerce.deliveryservice.repository.DeliveryRepository;
+import com.google.common.util.concurrent.AtomicDouble;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
@@ -13,10 +15,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import javax.annotation.PostConstruct;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @RestController
 @RequestMapping("")
@@ -33,8 +34,56 @@ public class HomeController {
     @Value("${message:Hello default}")
     private String message;
 
+    private AtomicDouble ref;
+
+    private Map<String, Integer> requestsLastMinute = new HashMap<>();
+
+    public HomeController(MeterRegistry registry) {
+        ref = registry.gauge("requests_per_second", new AtomicDouble(0.0f));
+    }
+
+    @PostConstruct
+    public void getSetup() {
+        //inicializar las 60 posiciones con 0s
+        for (int i = 0; i < 60; ++i) {
+            String key = Integer.toString(i);
+            requestsLastMinute.put(key.length() < 2 ? "0" + key : key, 0);
+        }
+        //actualizando campos de los segundos cada segundo
+        Timer t = new Timer();
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                //borrar el segundo que viene
+                String timeStamp = new SimpleDateFormat("ss").format(Calendar.getInstance().getTime());
+                requestsLastMinute.put(Integer.parseInt(timeStamp) + 1 < 60 ?
+                        Integer.toString(Integer.parseInt(timeStamp) + 1) : "00", 0);
+            }
+        }, 0, 1000);
+
+        //actualizando el valor de micrometer cada 3 segundos
+        Timer t1 = new Timer();
+        t1.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                //calcular las requests por segundo en el ultimo minuto
+                int counter = 0;
+                for (String key: requestsLastMinute.keySet()) {
+                    counter += requestsLastMinute.get(key);
+                }
+                ref.set(counter / (double) 60);
+            }
+        }, 0, 3000);
+    };
+
+    private synchronized void incrementCounter() {
+        String timeStamp = new SimpleDateFormat("ss").format(Calendar.getInstance().getTime());
+        requestsLastMinute.put(timeStamp, requestsLastMinute.get(timeStamp) + 1);
+    }
+
     @GetMapping("/hello")
     public ResponseEntity<String> getHello() {
+        incrementCounter();
         System.out.println(message);
         System.out.println(env.getProperty("message"));
         return new ResponseEntity<String>( env.getProperty("message"), HttpStatus.OK);
@@ -42,6 +91,7 @@ public class HomeController {
 
     @RequestMapping("/info")
     public String home() {
+        incrementCounter();
         // This is useful for debugging
         // When having multiple instance of gallery service running at different ports.
         // We load balance among them, and display which instance received the request.
@@ -50,22 +100,32 @@ public class HomeController {
 
     @GetMapping("")
     public List<Delivery> getDeliveries()    {
+        incrementCounter();
         return deliveryRepository.findAll();
     }
 
     //@HystrixCommand(fallbackMethod = "fallback")
     @GetMapping("/{id}")
     public Delivery getDelivery(@PathVariable final String id) {
-        return deliveryRepository.findById(id).get();
+        incrementCounter();
+        try {
+            return deliveryRepository.findById(id).get();
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Delivery not found"
+            );
+        }
     }
 
     @PostMapping("")
     public Delivery createInventory(@RequestBody Map<String, String> myJsonRequest) {
+        incrementCounter();
         return deliveryRepository.save(new Delivery(myJsonRequest.get("orderId").toString()));
     }
 
     @PutMapping("/{id}/nextEvent")
     public Delivery continueProcess(@PathVariable final String id) {
+        incrementCounter();
         Delivery delivery = deliveryRepository.findById(id).get();
         delivery.setNextDeliveryEvent();
         return deliveryRepository.save(delivery);
@@ -81,7 +141,7 @@ public class HomeController {
     // We'll add the logic of role based auth later
     @RequestMapping("/admin")
     public String homeAdmin() {
+        incrementCounter();
         return "This is the admin area of Delivery service running at port: " + env.getProperty("local.server.port");
     }
 }
-
--- order-service/src/main/java/com/ecommerce/orderservice/controller/HomeController.java
+++ order-service/src/main/java/com/ecommerce/orderservice/controller/HomeController.java
@@ -3,6 +3,8 @@ package com.ecommerce.orderservice.controller;
 import com.ecommerce.orderservice.model.Cart;
 import com.ecommerce.orderservice.model.Order;
 import com.ecommerce.orderservice.repository.OrderRepository;
+import com.google.common.util.concurrent.AtomicDouble;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -10,9 +12,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.annotation.PostConstruct;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @RestController
 @RequestMapping("")
@@ -29,8 +33,56 @@ public class HomeController {
     @Value("${message:Hello default}")
     private String message;
 
+    private AtomicDouble ref;
+
+    private Map<String, Integer> requestsLastMinute = new HashMap<>();
+
+    public HomeController(MeterRegistry registry) {
+        ref = registry.gauge("requests_per_second", new AtomicDouble(0.0f));
+    }
+
+    @PostConstruct
+    public void getSetup() {
+        //inicializar las 60 posiciones con 0s
+        for (int i = 0; i < 60; ++i) {
+            String key = Integer.toString(i);
+            requestsLastMinute.put(key.length() < 2 ? "0" + key : key, 0);
+        }
+        //actualizando campos de los segundos cada segundo
+        Timer t = new Timer();
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                //borrar el segundo que viene
+                String timeStamp = new SimpleDateFormat("ss").format(Calendar.getInstance().getTime());
+                requestsLastMinute.put(Integer.parseInt(timeStamp) + 1 < 60 ?
+                        Integer.toString(Integer.parseInt(timeStamp) + 1) : "00", 0);
+            }
+        }, 0, 1000);
+
+        //actualizando el valor de micrometer cada 3 segundos
+        Timer t1 = new Timer();
+        t1.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                //calcular las requests por segundo en el ultimo minuto
+                int counter = 0;
+                for (String key: requestsLastMinute.keySet()) {
+                    counter += requestsLastMinute.get(key);
+                }
+                ref.set(counter / (double) 60);
+            }
+        }, 0, 3000);
+    };
+
+    private synchronized void incrementCounter() {
+        String timeStamp = new SimpleDateFormat("ss").format(Calendar.getInstance().getTime());
+        requestsLastMinute.put(timeStamp, requestsLastMinute.get(timeStamp) + 1);
+    }
+
     @GetMapping("/hello")
     public ResponseEntity<String> getHello() {
+        incrementCounter();
         System.out.println(message);
         System.out.println(env.getProperty("message"));
         return new ResponseEntity<String>( env.getProperty("message"), HttpStatus.OK);
@@ -38,6 +90,7 @@ public class HomeController {
 
     @RequestMapping("/info")
     public String home() {
+        incrementCounter();
         // This is useful for debugging
         // When having multiple instance of gallery service running at different ports.
         // We load balance among them, and display which instance received the request.
@@ -46,22 +99,32 @@ public class HomeController {
 
     @GetMapping("")
     public List<Order> getDeliveries()    {
+        incrementCounter();
         return orderRepository.findAll();
     }
 
     //@HystrixCommand(fallbackMethod = "fallback")
     @GetMapping("/{id}")
     public Order getDelivery(@PathVariable final String id) {
-        return orderRepository.findById(id).get();
+        incrementCounter();
+        try {
+            return orderRepository.findById(id).get();
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Order not found"
+            );
+        }
     }
 
     @PostMapping("/")
     public Order createInventory(@RequestBody Cart cart) {
+        incrementCounter();
         return orderRepository.save(new Order(cart));
     }
 
     @PutMapping("/{id}/deliveryId")
     public Order setDeliveryId(@PathVariable final String id, @RequestBody Map<String, String> myJsonRequest) {
+        incrementCounter();
         Order order = orderRepository.findById(id).get();
         order.setDeliveryId(myJsonRequest.get("deliveryId").toString());
         return orderRepository.save(order);
@@ -78,6 +141,7 @@ public class HomeController {
     // We'll add the logic of role based auth later
     @RequestMapping("/admin")
     public String homeAdmin() {
+        incrementCounter();
         return "This is the admin area of Order service running at port: " + env.getProperty("local.server.port");
     }
 }
