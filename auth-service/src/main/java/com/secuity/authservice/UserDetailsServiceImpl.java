b7e6ad8830b4465d072cd2d082a66e5b30fcc940
MohamedAmarani <mohamedamarani@inslauro.net>
2021-04-26 19:47:35 +0200

    M

--- auth-service/src/main/java/com/secuity/authservice/UserDetailsServiceImpl.java
+++ auth-service/src/main/java/com/secuity/authservice/UserDetailsServiceImpl.java
@@ -1,9 +1,11 @@
 package com.secuity.authservice;
 
-import java.util.Arrays;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
+import com.google.common.util.concurrent.AtomicDouble;
 import com.secuity.authservice.model.AccountDTO;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -16,7 +18,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
 
 @Service   // It has to be annotated with @Service.
 public class UserDetailsServiceImpl implements UserDetailsService  {
@@ -27,9 +32,58 @@ public class UserDetailsServiceImpl implements UserDetailsService  {
     @Autowired
     private RestTemplate restTemplate;
 
+    private AtomicDouble ref;
+
+    private Map<String, Integer> requestsLastMinute = new HashMap<>();
+
+    public UserDetailsServiceImpl(MeterRegistry registry) {
+        ref = registry.gauge("requests_per_second", new AtomicDouble(0.0f));
+    }
+
+    @PostConstruct
+    public void getSetup() {
+        //inicializar las 60 posiciones con 0s
+        for (int i = 0; i < 60; ++i) {
+            String key = Integer.toString(i);
+            requestsLastMinute.put(key.length() < 2 ? "0" + key : key, 0);
+        }
+        //actualizando campos de los segundos cada segundo
+        Timer t = new Timer();
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                //borrar el segundo que viene
+                String timeStamp = new SimpleDateFormat("ss").format(Calendar.getInstance().getTime());
+                String timeStampIncremented = Integer.toString(Integer.parseInt(timeStamp) + 1);
+                requestsLastMinute.put(Integer.parseInt(timeStamp) + 1 < 60 ?
+                        (timeStampIncremented.length() < 2 ? "0" + timeStampIncremented : timeStampIncremented) : "00", 0);
+            }
+        }, 0, 1000);
+
+        //actualizando el valor de micrometer cada 3 segundos
+        Timer t1 = new Timer();
+        t1.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                //calcular las requests por segundo en el ultimo minuto
+                int counter = 0;
+                for (String key: requestsLastMinute.keySet()) {
+                    counter += requestsLastMinute.get(key);
+                    //System.out.println(key);
+                }
+                ref.set(counter / (double) 60);
+            }
+        }, 0, 3000);
+    };
+
+    private synchronized void incrementCounter() {
+        String timeStamp = new SimpleDateFormat("ss").format(Calendar.getInstance().getTime());
+        requestsLastMinute.put(timeStamp, requestsLastMinute.get(timeStamp) + 1);
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
+        incrementCounter();
         final ResponseEntity<List<AccountDTO>> res = restTemplate.exchange("http://account-service/",
                         HttpMethod.GET, null, new ParameterizedTypeReference<List<AccountDTO>>() {});
         final List<AccountDTO> users = res.getBody();
