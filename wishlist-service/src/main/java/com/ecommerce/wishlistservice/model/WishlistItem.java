5642599e1a7fc9c6e48aab8f9cad8857a069c892
MohamedAmarani <mohamedamarani@inslauro.net>
2021-07-02 18:34:43 +0200

    m

--- wishlist-service/src/main/java/com/ecommerce/wishlistservice/model/WishlistItem.java
+++ wishlist-service/src/main/java/com/ecommerce/wishlistservice/model/WishlistItem.java
@@ -2,20 +2,12 @@ package com.ecommerce.wishlistservice.model;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.util.Date;
 
 @ApiModel(description = "Details of a wishlist item")
 public class WishlistItem {
-    /*@ApiModelProperty(notes = "Unique id of the wishlist item")
-    @Id
-    String id;*/
     @ApiModelProperty(notes = "Unique id of the wishlist item product")
     private String productId;
-    @ApiModelProperty(notes = "Unique id of the inventory on which the wishlist item was created")
-    private String inventoryId;
     @ApiModelProperty(notes = "Target price for which a notification email will be sent to the user when reached")
     private double targetPrice;
     @ApiModelProperty(notes = "Creation date of the wishlist item")
@@ -24,20 +16,11 @@ public class WishlistItem {
     public WishlistItem() {
     }
 
-    public WishlistItem(String id, String productId, String inventoryId, double targetPrice, Date creationDate) {
+    public WishlistItem(String productId, double targetPrice, Date creationDate) {
         this.productId = productId;
-        this.inventoryId = inventoryId;
         this.targetPrice = targetPrice;
         this.creationDate = creationDate;
     }
-
-   /* public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    } */
 
     public String getProductId() {
         return productId;
@@ -45,14 +28,6 @@ public class WishlistItem {
 
     public void setProductId(String productId) {
         this.productId = productId;
-    }
-
-    public String getInventoryId() {
-        return inventoryId;
-    }
-
-    public void setInventoryId(String inventoryId) {
-        this.inventoryId = inventoryId;
     }
 
     public double getTargetPrice() {
