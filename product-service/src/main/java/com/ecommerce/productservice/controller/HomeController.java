e4438d994cc7ec1379e17a43b3b012bb41a8263b
MohamedAmarani <mohamedamarani@inslauro.net>
2021-08-05 17:22:39 +0200

    m

--- product-service/src/main/java/com/ecommerce/productservice/controller/HomeController.java
+++ product-service/src/main/java/com/ecommerce/productservice/controller/HomeController.java
@@ -101,6 +101,22 @@ public class HomeController {
         requestsLastMinute.put(timeStamp, requestsLastMinute.get(timeStamp) + 1);
     }
 
+    @InitBinder
+    public void initBinder(WebDataBinder binder) throws Exception {
+        final DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        final CustomDateEditor dateEditor = new CustomDateEditor(df, true) {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                if ("today".equals(text)) {
+                    setValue(new Date());
+                } else {
+                    super.setAsText(text);
+                }
+            }
+        };
+        binder.registerCustomEditor(Date.class, dateEditor);
+    }
+
     @GetMapping("/hello")
     public ResponseEntity<String> getHello() {
         incrementCounter();
