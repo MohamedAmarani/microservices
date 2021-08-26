760fb10d3a118f858d062bec2e98d009d91b8aad
MohamedAmarani <mohamedamarani@inslauro.net>
2021-08-26 20:08:34 +0200

    m

--- account-service/src/main/java/com/users/accountservice/Controller/HomeController.java
+++ account-service/src/main/java/com/users/accountservice/Controller/HomeController.java
@@ -443,7 +443,7 @@ public class HomeController {
         double reducedPercentage = 100 * ((productDTO.getCurrentPrice() - productDTO.getOriginalPrice()) / (double) productDTO.getOriginalPrice());
         DecimalFormat df = new DecimalFormat("#.##");
         reducedPercentage = Double.valueOf(df.format(reducedPercentage));
-        String text = "<h2>Hi " + receiver.getUsername() + ", a product that you wish has now a price within your target price!</h2>\n" +
+        String text = "<center><h2>Hi " + receiver.getUsername() + ", a product that you wish has now a price within your target price!</h2>\n" +
                 "<p style=\"font-size: 1.5em;\">The product " + productDTO.getName() + " of size " + productDTO.getSize() + " and id <strong style=\"background-color: #317399; padding: 0 5px; color: #fff;\">" + productDTO.getId() + "</strong> " +
                 "has changed its price from " + updatedProductInfo.get("oldPrice") + " EUR to " + productDTO.getCurrentPrice() + " EUR" + (Double.parseDouble(updatedProductInfo.get("oldPrice")) != productDTO.getOriginalPrice() ? " (" + reducedPercentage + "% from its original price of " + productDTO.getOriginalPrice() + " EUR)" : "") + ", matching the target price restriction of " +
                 updatedProductInfo.get("targetPrice") + " EUR that you established on your wishlist.</p>\n" +
--- product-service/src/main/java/com/ecommerce/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/ecommerce/productservice/ProductServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -18,6 +19,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableSwagger2
+@EnableHystrixDashboard
 
 public class ProductServiceApplication {
 
